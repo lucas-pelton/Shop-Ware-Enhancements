ded7d3019fd308dca56031edf2c980ca8d5da4a9
Lucas Pelton <info@somadg.com>
2022-12-16 11:52:16 -0800

    Sort and tag items when adding part in  edit mode

--- better-edit-parts.js
+++ better-edit-parts.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Better Edit Parts on Work Order
 // @namespace    Shop_Ware_Enhancements
-// @version      1.1
+// @version      2.0
 // @description  Add click to edit part
 // @author       Lucas Pelton @ MOM+POP, Ltd.
 // @match        *://*.shop-ware.com/work_orders*
@@ -16,10 +16,17 @@
  * REQUIRES Better Parts Search v1.3+
  */
 
-(function () {
+ (function () {
     'use strict';
 
     console.log('Better Edit Parts Running');
+
+
+    Number.prototype.between = function (a, b, inclusive = 0) {
+        return inclusive ?
+            (this >= Math.min(a, b) && this <= Math.max(a, b)) ? 1 : 0 :
+            (this > Math.min(a, b) && this < Math.max(a, b)) ? 1 : 0;
+    };
 
 
     /***************
@@ -32,6 +39,17 @@
 .backbone-app .modal-footer::after {
     display:none !important;
 }
+
+.in-stock {
+        background: #4caf503b;
+}
+
+.in-stock.limited-stock {
+    background: #FF22aa3b;
+}
+.no-stock a {
+    color: #d51515e0 !important;
+}
   `;
     document.head.appendChild(style);
 
@@ -39,17 +57,18 @@
     var $partsModal = $('.js-part-edit-modal-container'),
         actionString = "Edit Master Part";
 
+    var $addPartsSelector = $('#parts-autocomplete .ui-autocomplete');
+
 
     MutationObserver = window.MutationObserver || window.WebKitMutationObserver;
 
-    var observer = new MutationObserver(function (mutations, observer) {
+    var partsModalObserver = new MutationObserver(function (mutations, observer) {
         // fired when a mutation occurs
-        console.log(mutations, observer);
 
         let partNumber = $('#part_number', $partsModal).val(),
             url = `/inventory/?s=${partNumber}&modal=true`;
 
-        observer.disconnect($partsModal[0]);
+        partsModalObserver.disconnect($partsModal[0]);
         $('.modal-footer', $partsModal)
             .prepend(`<a target="_blank" href="${url}">${actionString}</a>`)
             .css({
@@ -60,7 +79,7 @@
         // Make this work to disappear the before and after items that are changing the spacing of the items in the container
         // $('.backbone-app .modal-footer:before, .backbone-app .modal-footer:after', $partsModal).css('display', 'none');
 
-        observer.observe($partsModal[0], {
+        partsModalObserver.observe($partsModal[0], {
             childList: true,
 
             //...
@@ -70,10 +89,48 @@
 
     // define what element should be observed by the observer
     // and what types of mutations trigger the callback
-    observer.observe($partsModal[0], {
+    partsModalObserver.observe($partsModal[0], {
         childList: true,
 
         //...
     });
+
+
+    var addPartsSelectorObserver = new MutationObserver(function (mutations) {
+        // fired when a mutation occurs
+        let $theUL = $(mutations[0].target);
+        if ($theUL.css('display') != 'block') return;
+
+        let theStuff = $("li", $theUL);
+
+
+        theStuff.sort(function (a, b) {
+            return parseInt($(a).find("i").text()) > parseInt($(b).find("i").text())
+                ? -1
+                : 1;
+        }).each(function () {
+            $(this).addClass(parseInt($('i', this).text()) ? "in-stock" : "no-stock");
+            $(this).addClass(parseInt(parseInt($('i', this).text()).between(0, 6)) ? "limited-stock" : "");
+        });
+
+
+        addPartsSelectorObserver.disconnect();
+        $theUL.append(theStuff);
+        $addPartsSelector.each(function(){
+        addPartsSelectorObserver.observe(this, {
+            childList: true,
+        });
+    });
+
+    });
+
+    // define what element should be observed by the observer
+    // and what types of mutations trigger the callback
+    $addPartsSelector.each(function(){
+        addPartsSelectorObserver.observe(this, {
+            childList: true,
+        });
+    });
+
 
 })();
